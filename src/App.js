f0a514619fa6fbcaadccb848dba55c9cab786e6e
HoudaMansouri <103124723+HoudaMansouri@users.noreply.github.com>
2022-06-13 15:49:21 +0200

    quete2fini

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import Simpsons from "./components/Simpsons";
+import { useState } from "react";
 
 function App() {
+  const [simpson, setSimpson] = useState("");
+
+  const getSimpsons = () => {
+    // Send the request
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        console.log(data);
+        setSimpson(data[0]);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Simpsons simpson={simpson} />
+      <button type="button" onClick={getSimpsons}>
+        Simpsons Citations
+      </button>
     </div>
   );
 }
---
+++ src/components/Simpsons.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Simpsons = ({ simpson }) => {
+  return (
+    <div className="Simpsons">
+      <img src={simpson.image} />
+      <h1>Name : {simpson.character}</h1>
+      <p>Quote : {simpson.quote}</p>
+    </div>
+  );
+};
+
+export default Simpsons;
